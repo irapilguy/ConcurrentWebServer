fa85e0a911c3b20ae87c9a5bcdd9d3f3531ef803
Iryna Pilhui <irapilguy@gmail.com>
2024-06-06 11:52:27 +0200

    Send a client request to the server from the main.

--- ConcurrentWebServer/ConcurrentWebServer/main.cpp
+++ ConcurrentWebServer/ConcurrentWebServer/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 
 #include "Server.h"
+#include "Client.h"
 
 int main()
 {
-    Server server("127.0.0.1", 8080, std::thread::hardware_concurrency());
-    server.start();
+    std::string serverAddress = "127.0.0.1";
+    int port = 8080;
+    Server server(serverAddress, port, std::thread::hardware_concurrency() - 1);
+
+    //server.start();
+   std::thread serverThread([&server] { server.start(); });
+
+    // Give the server a moment to start up
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    Client client(serverAddress, port);
+    if (client.connectToServer()) {
+        std::cout << "Client connected to server\n";
+        std::string request = "Hello from client";
+        std::string response = client.sendRequest(request);
+        std::cout << "Client received response: " << response << std::endl;
+    }
+    else {
+        std::cerr << "Failed to connect to server\n";
+    }
+
+    server.stop();
+    if(serverThread.joinable())
+        serverThread.join();
+
     return 0;
 }
